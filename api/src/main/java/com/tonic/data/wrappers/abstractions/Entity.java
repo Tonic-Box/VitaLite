3a270897e4f52ace40b59da3a50121033f052707
TonicBox <gsec.tonicbox@protonmail.com>
2025-11-14 02:12:41 -0600

    Static .search() to Entity class

--- api/src/main/java/com/tonic/data/wrappers/abstractions/Entity.java
+++ api/src/main/java/com/tonic/data/wrappers/abstractions/Entity.java
@@ -1,5 +1,11 @@
 package com.tonic.data.wrappers.abstractions;
 
+import com.tonic.queries.combined.EntityQuery;
+
 public interface Entity extends Locatable, Interactable
 {
+    static EntityQuery search()
+    {
+        return new EntityQuery();
+    }
 }
