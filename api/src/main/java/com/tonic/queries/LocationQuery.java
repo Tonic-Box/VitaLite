fd4ded04ecf9299df030f4c5d7caa66912fbcfd3
TonicBox <gsec.tonicbox@protonmail.com>
2025-11-14 14:45:45 -0600

    LocationQuery

--- api/src/main/java/com/tonic/queries/LocationQuery.java
+++ api/src/main/java/com/tonic/queries/LocationQuery.java
@@ -14,33 +14,67 @@ import net.runelite.api.coords.WorldPoint;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * A query class for filtering and manipulating Tile locations in the game world.
+ */
 public class LocationQuery extends AbstractQuery<Tile, LocationQuery>
 {
+    /**
+     * Constructs a new LocationQuery instance, initializing it with all tiles from the game manager.
+     */
     public LocationQuery() {
         super(GameManager.getTiles());
     }
 
+    /**
+     * Filters the query to only include tiles that are reachable from the player's current position.
+     *
+     * @return LocationQuery
+     */
     public LocationQuery isReachable() {
         Tile player = Location.toTile(PlayerEx.getLocal().getWorldPoint());
         keepIf(tile -> Location.isReachable(player, tile));
         return this;
     }
 
+    /**
+     * Filters the query to only include tiles that have a line of sight to the player's current position.
+     *
+     * @return LocationQuery
+     */
     public LocationQuery hasLosTo() {
         Tile player = Location.toTile(PlayerEx.getLocal().getWorldPoint());
         return keepIf(tile -> Location.hasLineOfSightTo(player, tile));
     }
 
+    /**
+     * Filters the query to only include tiles within a specified distance from the player.
+     *
+     * @param distance The maximum distance from the player.
+     * @return LocationQuery
+     */
     public LocationQuery withinDistance(int distance) {
         Tile player = Location.toTile(PlayerEx.getLocal().getWorldPoint());
         return keepIf(tile -> Location.getDistance(player, tile) <= distance);
     }
 
+    /**
+     * Filters the query to exclude tiles within a specified distance from the player.
+     *
+     * @param distance The minimum distance from the player.
+     * @return LocationQuery
+     */
     public LocationQuery beyondDistance(int distance) {
         Tile player = Location.toTile(PlayerEx.getLocal().getWorldPoint());
         return removeIf(tile -> Location.getDistance(player, tile) <= distance);
     }
 
+    /**
+     * Filters the query to only include tiles that are within a specified pathing distance from the player.
+     *
+     * @param distance The maximum pathing distance from the player.
+     * @return LocationQuery
+     */
     public LocationQuery withinPathingDistance(int distance) {
         Tile player = Location.toTile(PlayerEx.getLocal().getWorldPoint());
         return keepIf(tile -> {
@@ -49,6 +83,12 @@ public class LocationQuery extends AbstractQuery<Tile, LocationQuery>
         });
     }
 
+    /**
+     * Filters the query to exclude tiles that are within a specified pathing distance from the player.
+     *
+     * @param distance The minimum pathing distance from the player.
+     * @return LocationQuery
+     */
     public LocationQuery beyondPathingDistance(int distance) {
         Tile player = Location.toTile(PlayerEx.getLocal().getWorldPoint());
         return removeIf(tile -> {
@@ -57,16 +97,33 @@ public class LocationQuery extends AbstractQuery<Tile, LocationQuery>
         });
     }
 
+    /**
+     * Filters the query to only include tiles within a specified WorldArea.
+     *
+     * @param area The WorldArea to check against.
+     * @return LocationQuery
+     */
     public LocationQuery withinArea(WorldArea area)
     {
         return keepIf(tile -> area.contains(tile.getWorldLocation()));
     }
 
+    /**
+     * Filters the query to exclude tiles within a specified WorldArea.
+     *
+     * @param area The WorldArea to check against.
+     * @return LocationQuery
+     */
     public LocationQuery outsideArea(WorldArea area)
     {
         return removeIf(tile -> area.contains(tile.getWorldLocation()));
     }
 
+    /**
+     * Filters the query to only include tiles that contain any tile objects (decorative, game, ground, or wall).
+     *
+     * @return LocationQuery
+     */
     public LocationQuery hasTileObject()
     {
         return keepIf(tile ->
@@ -77,11 +134,21 @@ public class LocationQuery extends AbstractQuery<Tile, LocationQuery>
         );
     }
 
+    /**
+     * Converts the collected tiles to a list of WorldPoint locations.
+     *
+     * @return List of WorldPoint locations.
+     */
     public List<WorldPoint> toWorldPointList()
     {
         return Static.invoke(() -> collect().stream().map(Tile::getWorldLocation).collect(Collectors.toList()));
     }
 
+    /**
+     * Converts the collected tiles to a list of LocalPoint locations.
+     *
+     * @return List of LocalPoint locations.
+     */
     public List<LocalPoint> toLocalPointList()
     {
         return Static.invoke(() -> collect().stream().map(Tile::getLocalLocation).collect(Collectors.toList()));
