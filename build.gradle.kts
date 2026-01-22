15b5a829ef7dac170109e6da869a41fe030837b2
TonicBox <gsec.tonicbox@protonmail.com>
2026-01-21 18:09:53 -0600

    # 1.12.13_0 - subrev update

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
     id("maven-publish")
 }
 
-val vitaVersion by extra("2")
-val runeliteVersion by extra("1.12.12.1")
+val vitaVersion by extra("0")
+val runeliteVersion by extra("1.12.13")
 
 group = "com.tonic"
 version = runeliteVersion + "_" + vitaVersion
@@ -358,9 +358,9 @@ tasks.register<Exec>("publishRelease") {
     dependsOn("buildRelease")
 
     val tag = "${runeliteVersion}_${vitaVersion}"
-    val title = "${tag}-feature"
+    val title = "${tag}-subrev"
     val body = "# ${tag}\n" +
-            "- Proxy now displays in client title bar again";
+            "- subrev update\n"
     val zipFile = layout.buildDirectory.file("libs/VitaLite-${project.version}.zip").get().asFile
 
     doFirst {
